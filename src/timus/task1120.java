a28f6e068a841c9ca01fd5befd21435fe657aa75
DanilAlexandrovich <vosmerka97@mail.ru>
2023-01-10 19:10:06 +0500

    initial commit

---
+++ src/timus/task1120.java
@@ -0,0 +1,23 @@
+package timus;
+
+import java.util.Scanner;
+
+public class task1120 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int N2 = N * 2;
+        int NSQRT = (int) Math.ceil(Math.sqrt(N2));
+
+        for (int p = NSQRT; p >= 1; p--) {
+            if (N2 % p == 0) {
+                int b = (N2 / p) - p + 1;
+                if (b > 0 && b % 2 == 0) {
+                    int a = b / 2;
+                    System.out.printf("%d %d%n", a, p);
+                    return;
+                }
+            }
+        }
+    }
+}
